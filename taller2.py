d5f6e14077a6653d9968aa27dbf208d90e6a0109
jdauza14 <jd.auza@uniandes.edu.co>
2024-08-20 11:25:00 -0500

    nuevo cambio histograma

--- taller2.py
+++ taller2.py
@@ -4,3 +4,12 @@ sigma =0.5
 n =2000
 val = np.random.normal(loc=mu, scale=sigma, size=n)
 print (val)
+import matplotlib.pyplot as plt
+count, bins, ignored = plt.hist(x=vals, bins=30)
+plt.title("histograma")
+plt.xlabel("tiempo")
+plt.xlabel("frecuencia")
+plt.show
+
+print(count)
+print(bins)
