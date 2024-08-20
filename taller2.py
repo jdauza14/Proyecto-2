85dd41e446a4b9c08264e4bf5a15aa672032e908
jdauza14 <jd.auza@uniandes.edu.co>
2024-08-20 11:29:06 -0500

    histograma corregido

--- taller2.py
+++ taller2.py
@@ -5,7 +5,7 @@ n =2000
 val = np.random.normal(loc=mu, scale=sigma, size=n)
 print (val)
 import matplotlib.pyplot as plt
-count, bins, ignored = plt.hist(x=vals, bins=30)
+count, bins, ignored = plt.hist(x=val, bins=30)
 plt.title("histograma")
 plt.xlabel("tiempo")
 plt.xlabel("frecuencia")
